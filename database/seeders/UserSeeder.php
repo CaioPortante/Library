73b626a79ebb5bba9cc999bed16cba4fbb24d43d
Caio Barbosa Portante <caioportante@gmail.com>
2024-05-18 13:00:48 -0300

    :sparkles: Feat: Create UserType and User seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin'),
+            'type' => 1,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
