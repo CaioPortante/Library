f9875781fd3c26f0febc680725734f53cda3e439
Caio Barbosa Portante <caioportante@gmail.com>
2024-05-18 12:28:34 -0300

    :sparkles: Feat: Create migrations and Models

---
+++ database/migrations/2024_05_18_151518_add_loans_to_books_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('loans', function (Blueprint $table) {
+            $table->unsignedBigInteger("book");
+            $table->foreign('book')->references('id')->on('books')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('loans', function (Blueprint $table) {
+            $table->dropForeign(['book']);
+            $table->dropColumn('book');
+        });
+    }
+};
