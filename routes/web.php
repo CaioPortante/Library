c3acef503cbdb1be3ec5f5a34812aca646b84345
Caio Barbosa Portante <caioportante@gmail.com>
2024-05-18 21:08:29 -0300

    :sparkles: Feat: Initial css config

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome'); // Initial Laravel Page
+    return view('index');
 });
