06b3a581a4495d2a2409d8d05394b28b50b5bca4
Caio Barbosa Portante <caioportante@gmail.com>
2024-05-19 11:05:01 -0300

    :sparkles: Feat: create books seeder

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.app')
 
 @section('title', 'Admin Books')
-@section('pageTitle', 'Admin Books')
+@section('pageTitle', 'Controle de Livros')
 
 @section('content')
-    
+    <table>
+        
+    </table>
 @endsection
