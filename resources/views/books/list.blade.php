d0ce8ce1be865e1c4943a1bc03e78d64a0363aef
Caio Barbosa Portante <caioportante@gmail.com>
2024-05-23 12:04:31 -0300

    :sparkles: Feat: books search

--- resources/views/books/list.blade.php
+++ resources/views/books/list.blade.php
@@ -5,31 +5,27 @@
 
 @section('content')
 
-    <a href={{ route('dashboard') }}>
-        <button type="button" class="ml-8 mb-4 text-white bg-gray-600 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-            Voltar
-        </button>
-    </a>
-    <div class="grid grid-cols-4 gap-4">
-        @foreach ($books as $book)
-            @if ($book->quantity <= 0)
-                <a class="relative block max-w-sm mx-auto p-6 bg-gray-200 border border-gray-400 rounded-lg shadow">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-700/75">{{ $book->title }}</h5>
-                    <p class="font-normal text-gray-500/75 mb-2">{{ $book->description }}</p>
-                    <p class="font-normal text-gray-400/75 text-sm absolute bottom-2 right-2">{{ $book->isbn }}</p>
-                    <p class="font-normal text-red-400 text-sm absolute bottom-2 left-8">Indisponível</p>
-                </a>
-            @else
-                <a href="{{ route('loan.book', ["id" => $book->id]) }}" 
-                    class="relative block max-w-sm mx-auto p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-400/10 hover:border-gray-600 transition"
-                    >
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ $book->title }}</h5>
-                    <p class="font-normal text-gray-700">{{ $book->description }}</p>
-                    <p class="font-normal text-gray-400 text-sm absolute bottom-2 right-2">{{ $book->isbn }}</p>
-                    <p class="font-normal text-gray-400 text-sm absolute bottom-2 left-8">{{ $book->quantity }} Disponíveis</p>
-                </a>
-            @endif 
-        @endforeach
-    </div>   
+    <div class="flex flex-row justify-between px-8">
+        <a href={{ route('dashboard') }}>
+            <button type="button" class="mb-4 text-white bg-gray-600 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                Voltar
+            </button>
+        </a>
+
+        <div class="pr-8 w-1/4">
+            <input name="search" id="search" type="text" placeholder="Buscar um Livro/Autor/ISBN" class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5">
+        </div>
+    </div>
+    <div id="response-div" class="mt-8">
+
+    </div>
+
+    @push('scripts')
+        <script>
+            $('#search').on('keyup', (input) => {
+                searchBooks(input.target.value)
+            })
+        </script>
+    @endpush
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
     <title>@yield('title', 'Library')</title>
     <link rel="icon" href="{{ asset('img/icon_books.png') }}" type="image/x-icon">
 
@@ -26,12 +28,18 @@
 
         
     </div>
-    
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+    <script src="{{ asset('js/searchBooks.js') }}"></script>
+
     @if (!empty(session('response')))
     
         <script src="{{ asset('js/error.js') }}"></script>
 
     @endif
+    
+    @stack('scripts')
 
 </body>
 </html>
